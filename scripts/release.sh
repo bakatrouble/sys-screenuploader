e4390619aee7da031d236030df8e7c74fd333784
bakatrouble <bakatrouble@gmail.com>
2020-01-19 13:21:56 +0300

    fixes #1 (use atmosphere contents), will be used in next release

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,9 +4,9 @@ rm -rf release
 mkdir release
 cd release
 mkdir -p config/sys-screenuploader
-mkdir -p atmosphere/titles/42000062616B6101/flags
+mkdir -p atmosphere/contents/42000062616B6101/flags
 cp ../config.ini config/sys-screenuploader/config.ini
-cp ../toolbox.json atmosphere/titles/42000062616B6101/toolbox.json
-cp ../cmake-build-release/screenuploader.nsp atmosphere/titles/42000062616B6101/exefs.nsp
-touch atmosphere/titles/42000062616B6101/flags/boot2.flag
+cp ../toolbox.json atmosphere/contents/42000062616B6101/toolbox.json
+cp ../cmake-build-release/screenuploader.nsp atmosphere/contents/42000062616B6101/exefs.nsp
+touch atmosphere/contents/42000062616B6101/flags/boot2.flag
 zip -r sys-screenuploader.zip *
